2af4ae15217a77cb588f858f34609453071c1f4f
Frederic LeBel <frederic.lebel@gmail.com>
2012-05-16 15:37:49 -0400

    Support text selecting in the output window.
    - Useful for copying the entire output or just a single line.

--- WPF_FrontEnd/ColoredCombinatorTextBlock.xaml.cs
+++ WPF_FrontEnd/ColoredCombinatorTextBlock.xaml.cs
@@ -28,20 +28,23 @@ namespace WPF_FrontEnd
         {
             public string glyphs;
             public StringBuilder text = new StringBuilder();
-            public TextBlock textBlock;
+            public TextBox textBlock;
         }
 
         List<ColorScheme> _colorSchemes = new List<ColorScheme>();
         ColorScheme _defaultScheme;
+        ColorScheme _completeScheme;
 
         public void InitializeColorScheme()
         {
             Func<string, Brush, ColorScheme> makeColorScheme = (str, brush) =>
                 {
-                    var tb = new TextBlock();
-                    tb.Background = Brushes.Transparent;
-                    tb.Foreground = brush;
-                    tb.FontFamily = new FontFamily("Consolas");
+                    var tb = new TextBox()
+                    {
+                        Background = Brushes.Transparent,
+                        Foreground = brush,
+                        FontFamily = new FontFamily("Consolas")
+                    };
                     return new ColorScheme()
                     {
                         glyphs = str,
@@ -55,12 +58,14 @@ namespace WPF_FrontEnd
             _colorSchemes.Add(makeColorScheme("I", Brushes.Red));
             _defaultScheme = makeColorScheme("", Brushes.Black);
             _defaultScheme.textBlock.Background = Brushes.White;
+            _completeScheme = makeColorScheme("", Brushes.Transparent);
 
             ContentSpace.Children.Add(_defaultScheme.textBlock);
             foreach (var scheme in _colorSchemes)
             {
                 ContentSpace.Children.Add(scheme.textBlock);
             }
+            ContentSpace.Children.Add(_completeScheme.textBlock);
         }
 
         private string _text;
@@ -78,6 +83,7 @@ namespace WPF_FrontEnd
                 foreach (var scheme in _colorSchemes)
                     scheme.text.Clear();
                 _defaultScheme.text.Clear();
+                _completeScheme.text.Clear();
 
                 foreach (char c in _text)
                 {
@@ -110,12 +116,14 @@ namespace WPF_FrontEnd
                         _defaultScheme.text.Append(' ');
                     }
                 }
+                _completeScheme.text.Append(_text);
 
                 foreach (var scheme in _colorSchemes)
                 {
                     scheme.textBlock.Text = scheme.text.ToString();
                 }
                 _defaultScheme.textBlock.Text = _defaultScheme.text.ToString();
+                _completeScheme.textBlock.Text = _completeScheme.text.ToString();
             }
         }
     }
